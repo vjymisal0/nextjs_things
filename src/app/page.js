f00ee518f959572d43a54f120cdb37da15b6d9c4
Vijay Misal <misalvijay153@gmail.com>
2024-07-10 22:05:30 +0530

    conditional styling

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,16 @@
 "use client";
-import React from 'react';
+import React, { useState } from 'react';
 import custom from './custom.module.css'
-
+import style from './style.module.css'
 
 const NewComp = () => {
 
+  const [color, setColor] = useState("gray")
 
   return (
     <div>
-      <h1 className={custom.headingtag}>Fetch Data with API in client component</h1>
+      <h1 className={color == "red" ? style.red : style.gray}>Fetch Data with API in client component</h1>
+      <button onClick={() => { setColor("red") }}>Click to change color of above text</button>
       <h1>Hello this is inline css integration in Next.js</h1>
     </div>
   );
